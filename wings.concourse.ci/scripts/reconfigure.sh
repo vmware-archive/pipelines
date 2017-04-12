b0314126c5081da96e12ab4592cf72a6afeb17ea
Alex Suraci <suraci.alex@gmail.com>
2017-04-12 12:07:03 -0400

    move release pipelines to wings

---
+++ wings.concourse.ci/scripts/reconfigure.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+check_installed() {
+  if ! command -v $1 > /dev/null 2>&1; then
+    printf "$1 must be installed before running this script!"
+    exit 1
+  fi
+}
+
+configure_pipeline() {
+  local name=$1
+  local pipeline=$2
+
+  printf "configuring the $name pipeline...\n"
+
+  fly -t wings set-pipeline \
+    -p $name \
+    -c $pipeline \
+    -l <(lpass show "Concourse Pipeline Credentials" --notes)
+}
+
+check_installed lpass
+check_installed fly
+
+# Make sure we're up to date and that we're logged in.
+lpass sync
+
+pipelines_path=$(cd $(dirname $0)/../ && pwd)
+
+pipeline=${1}
+
+for release in $pipelines_path/releases/concourse-*.yml; do
+  version=$(echo $release | sed -e 's/.*concourse-\(.*\).yml$/\1/')
+  configure_pipeline releases:$version \
+    $release
+done

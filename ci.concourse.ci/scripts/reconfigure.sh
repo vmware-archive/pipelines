5d6f6cec8e6aa16fb43474b747e05b32061c6c32
Alex Suraci <asuraci@pivotal.io>
2017-06-14 14:06:37 -0400

    Revert "first pass at reconfiguring pipelines dynamically"
    This reverts commit b737b9dd2795fecc78f19691f201e9cea75d2b32.

--- ci.concourse.ci/scripts/reconfigure.sh
+++ ci.concourse.ci/scripts/reconfigure.sh
@@ -19,14 +19,10 @@ configure_pipeline() {
   fly -t ci set-pipeline \
     -p $name \
     -c $pipeline \
-    -l <(lpass show "Concourse Pipeline Credentials" --notes) \
-    -v basic-auth-username=<(credhub get -n /concourse-prod-bosh/concourse-prod/basic_auth_username --output-json | jq -r .value) \
-    -v basic-auth-password=<(credhub get -n /concourse-prod-bosh/concourse-prod/basic_auth_password --output-json | jq -r .value)
+    -l <(lpass show "Concourse Pipeline Credentials" --notes)
 }
 
 check_installed lpass
-check_installed credhub
-check_installed jq
 check_installed fly
 
 # Make sure we're up to date and that we're logged in.
@@ -47,6 +43,32 @@ if [ "$#" -gt 0 ]; then
       $file
   done
 else
-  configure_pipeline reconfigure-pipelines \
-    $pipelines_path/reconfigure-pipelines.yml
+  configure_pipeline main \
+    $pipelines_path/concourse.yml
+
+  if [ -e $pipelines_path/releases/concourse-*.yml ]; then
+    for release in $pipelines_path/releases/concourse-*.yml; do
+      version=$(echo $release | sed -e 's/.*concourse-\(.*\).yml$/\1/')
+      configure_pipeline releases:$version \
+        $release
+    done
+  fi
+
+  configure_pipeline wings \
+    $pipelines_path/wings.yml
+
+  configure_pipeline resources \
+    $pipelines_path/resources.yml
+
+  configure_pipeline images \
+    $pipelines_path/images.yml
+
+   configure_pipeline tracksuit \
+     $pipelines_path/tracksuit.yml
+
+  configure_pipeline hangar \
+    $pipelines_path/hangar.yml
+
+  configure_pipeline prs \
+    $pipelines_path/pull-requests.yml
 fi

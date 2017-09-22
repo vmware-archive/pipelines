70807f2a832825948cbec24fb0daf128be97e598
JT Archie <jtarchie@gmail.com>
2017-09-22 12:29:04 -0600

    configure concourse-pipeline-resource with fly

--- ci.concourse.ci/resources/reconfigure.sh
+++ ci.concourse.ci/resources/reconfigure.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -ex
+set -eux
 
-resources=${1:-"pool time git docker-image"}
+resource=$1; shift
+params=${*:-"ship=true"}
 
-for resource in $resources; do
-  fly -t resources set-pipeline -p "$resource" -c template.yml -v resource="$resource"
-  fly -t resources expose-pipeline -p "$resource"
-done
+usage() {
+  echo "usage: $0 <resource-name> [ship=true] [resources=fly|cf]"
+  exit 1
+}
+
+[ -n "$resource" ] || usage
+
+fly -t resources set-pipeline -p "$resource" -c <(erb $params template.yml.erb) -v resource="$resource"
+fly -t resources expose-pipeline -p "$resource"

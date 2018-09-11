399d87073e56cb64db03031d78b6ad49f888f546
Alex Suraci <suraci.alex@gmail.com>
2018-09-11 17:21:09 -0400

    unify all resource pipelines into one template
    jsonnet is pretty jsonneat

--- ci.concourse-ci.org/resources/reconfigure.sh
+++ ci.concourse-ci.org/resources/reconfigure.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-set -eu
+set -e -u
 
-vanilla_resources="git time registry-image bosh-io-release bosh-io-stemcell tracker hg github-release semver pool"
-privileged_resources="docker-image"
-special_resources="s3 cf concourse-pipeline"
-
-for r in $vanilla_resources; do
-  echo "--- configuring $r ---"
-  fly -t resources sp -p $r -c template.yml -v resource=$r -v privileged=false
-  echo ""
-done
-
-for r in $privileged_resources; do
-  echo "--- configuring $r ---"
-  fly -t resources sp -p $r -c template.yml -v resource=$r -v privileged=true
-  echo ""
-done
-
-for r in $special_resources; do
-  echo "--- configuring $r ---"
-  fly -t resources sp -p $r -c $r.yml
+cat resources | while read resource; do
+  echo "configuring $resource..."
+  fly -t resources sp -p $resource -c <(jsonnet -V resource=$resource template.jsonnet)
   echo ""
 done

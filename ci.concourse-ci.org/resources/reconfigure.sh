3ddc8cf36ca70a2700e76b5fef496daf075db5fa
Sameer Vohra <svohra@pivotal.io>
2019-04-04 14:25:51 -0400

    Remove redundant ci.concourse-ci.org resources
    - these ci artifacts are obsolete and their new location is in
      the concourse repo
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

--- ci.concourse-ci.org/resources/reconfigure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -u
-
-for resource in $(cat resources); do
-  echo "configuring $resource..."
-  fly -t resources sp -p $resource -c <(jsonnet -V resource=$resource template.jsonnet)
-  echo ""
-done
